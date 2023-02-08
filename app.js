8b04d1156e82a5dec03f03539bbfbec1b249a466
KaiqueSousa <kaique.una@gmail.com>
2023-02-08 10:01:54 -0300

    Update app.js
    ajustes

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var options = {
   apis: ["./routes/user.js", "./routes/contato.js"],
 };
 var swaggerSpec = swaggerJSDoc(options);
-
+const port = process.env.PORT || 3001;
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -80,5 +80,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+app.listen(port);
 
 module.exports = app;
