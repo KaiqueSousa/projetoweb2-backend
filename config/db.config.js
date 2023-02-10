6af5394e18b9d4ba609dee00ebe5736dd7cde3cc
KaiqueSousa <kaique.una@gmail.com>
2023-02-10 14:06:26 -0300

    Link banco de dados

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv').config();
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+//const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const url = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:7058/${DB_NAME}`;
+const url = `postgresql://postgres:xegAmMmOY2jHPdUtzv5N@containers-us-west-188.railway.app:7058/railway`;
 const sequelize = new Sequelize(url);
 const db = {};
 
