df0e37ba836edf9971805282a4fb2f9dbfe1c26c
KaiqueSousa <kaique.una@gmail.com>
2023-02-10 14:20:52 -0300

    varialvel do banco

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,7 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv').config();
 
-//const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-
-const url = `postgresql://postgres:xegAmMmOY2jHPdUtzv5N@containers-us-west-188.railway.app:7058/railway`;
+const url = `${process.env.DB_URL}`;
 const sequelize = new Sequelize(url);
 const db = {};
 
